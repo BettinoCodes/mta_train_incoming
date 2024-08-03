1c14363b11209144e88e9ca95c71a9a753c4775c
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-08-03 16:53:12 -0400

    Update and rename main.py to mta_train_notify.py

--- mta_train_notify.py
+++ mta_train_notify.py
@@ -1,5 +1,6 @@
 import requests, time
 from datetime import datetime, timedelta
+#Need to import twillio
 from twilio.rest import Client
 import os
 from dotenv import load_dotenv
@@ -35,11 +36,11 @@ if len(notification_message) == 0:
     print("There are no trains at this time")
 else:
     for i in range(int(len(firstStopTime)/2)):
-        client = Client(os.getenv("ACCOUNT_SID"), os.getenv("AUTH_TOKEN"))
+        client = Client(os.environ["account_sid"], os.environ["auth_token"])
         message = client.messages.create(
-            from_=f'whatsapp:+{os.getenv("TWILIO_NUMBER")}',
+            from_=f'whatsapp:+{os.environ["twilio_number"]}',
             body=notification_message[i],
-            to=F'whatsapp:+{os.getenv("MY_PHONE_NUMBER")}'
+            to=F'whatsapp:+{os.environ["my_number"]}'
         )
         print(message.status)
         print(message.sid)
