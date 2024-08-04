26226a3516f995c7c6a29c6b1e704b59cfd3e546
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-08-03 21:18:50 -0400

    Update notification.py

--- notification.py
+++ notification.py
@@ -4,14 +4,14 @@ import os
 
 class NotificationManager:
     """
-    this class is made to manage this data when it comes to the notifications, and having all of the necessary 
+    this class is made to manage this data when it comes to notifications, and having all of the necessary 
     attributes related to sending them
     """
-    def __init__(self) -> None:
-          self.acc_sid = os.environ["ACCOUNT_SID"]
-          self.auth_token = os.environ["AUTH_TOKEN"]
-          self.my_phone = os.environ["MY_PHONE_NUMBER"]
-          self.twilio_number = os.environ["TWILIO_NUMBER"]
+    def __init__(self):
+          self.acc_sid = os.environ["account_sid"]
+          self.auth_token = os.environ["auth_token"]
+          self.my_phone = os.environ["my_phone_number"]
+          self.twilio_number = os.environ["twilio_number"]
 
     def send_messages_from_list(self, list_of_times):
         """This function is meant to send all of the data in the list individually as a message"""
